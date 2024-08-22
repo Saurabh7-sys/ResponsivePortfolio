15c62161b656103cb61b99c0479b7cfe9300e5d4
Saurabh7-sys <saurabhshelar512@gmail.com>
2024-08-22 19:47:48 +0530

    Your commit message

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link , useNavigate } from 'react-router-dom';
-import Button from './button';
+import Button from './Button';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
